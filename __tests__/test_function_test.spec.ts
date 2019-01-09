7e6643f1743201ef44fb85f7d3c1881b8fe6216e
Martin Woelfer <17739109+mwoelfer-tgm@users.noreply.github.com>
2019-01-09 16:24:59 +0100

    moved __tests__ folder

--- __tests__/test_function_test.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test_function } from "../src";
-import { expect } from "chai"
-
-describe("This is a test", () => {
-    it("further testing", () => {
-        expect(test_function()).to.eql("test_string")
-    })
-})
